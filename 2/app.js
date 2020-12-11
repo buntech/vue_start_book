a6dd593e9e3b8b5211076fe02f05c89b720fc4d0
buntech <yu.sskabc.my123@gmail.com>
2020-12-11 14:45:37 +0900

    ディレクティブまで(クラス,スタイル,リスト,イベント)

--- 2/app.js
+++ 2/app.js
@@ -12,7 +12,7 @@ var items = [
   {
     name: '消しゴム',
     price: 500,
-    quantity: 0
+    quantity: 1
   }
 ]
 
@@ -41,8 +41,19 @@ var vm = new Vue(
         return Math.floor(this.totalPrice * 1.08)
       },
       canBuy: function () {
-        return this.totalPrice >= 1000
+        return this.totalPrice >= 1500  // 1500円以上で購入可能
       },
+      // errorMessageClass: function () {
+      //   return {
+      //     error: !this.canBuy
+      //   }
+      // },
+      errorMessageStyle: function () {
+        return {
+          border: this.canBuy ? '' : '1px solid red',
+          color: this.canBuy ? '' : 'red'
+        }
+      }
     }
   },
   {
